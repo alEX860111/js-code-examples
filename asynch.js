fd6b0d0dd6f58ad21e546fb42742310df5d4113b
Alexander Buhr <alex86011119@googlemail.com>
2016-01-27 19:50:52 +0100

    generator example

--- asynch.js
+++ asynch.js
@@ -23,3 +23,20 @@ function asynch() {
 asynch().then(function(res) {
 	console.log(res);
 });
+
+// cb + generator
+
+function asynch() {
+	setTimeout(function() {
+		iterator.next("hello world");
+	}, 500);
+}
+
+
+function* main() {
+	var res = yield asynch();
+	console.log(res);
+}
+
+var iterator = main();
+iterator.next();
